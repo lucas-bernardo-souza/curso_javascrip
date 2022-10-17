55b0786e0ba8fa450c55853d841cf3bd6776c8de
Lucas Bernardo de Souza <lucasbernardodesouza510@gmail.com>
2022-10-17 12:03:27 -0300

    concatenacao

---
+++ concatenacao.js
@@ -0,0 +1,6 @@
+//var nome = 'Maria';
+var nome = prompt('Digite o seu nome: ');
+var idade = prompt('Digite a sua idade: ');
+// prompt sempre recebe valores como string
+
+document.write('<h1>Olá '+ nome + ', tudo bem? Você tem '+ idade + ' anos</h1>');
