4ca0873f0ec46a8c9e23554a4313efd90cad9fd7
Lucas Bernardo de Souza <lucasbernardodesouza510@gmail.com>
2022-10-17 11:52:57 -0300

    variaveis

---
+++ variaveis/script.js
@@ -0,0 +1,18 @@
+//string
+var texto = "Curso de JavaScript";
+
+//number
+// em javascript variáveis do tipo number armazenam inteiros e fracionários
+var numeroInteiro = -7;
+var numeroFracionado = 10.75;
+
+//boolean
+var teste = true;
+var teste1 = false;
+
+// a inferencia do tipo é aplicada automaticamente na variavel ao fazer a atribuição de um valor
+
+alert(numeroFracionado); // Não é muito utilizado em produçõa, mas é importante para debug
+
+document.write(texto); // Podemos escrever valores dentro do browser
+console.log(numeroInteiro);
